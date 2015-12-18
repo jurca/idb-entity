0fb70ce074cdb6932c0f7b4cba14ddddf2fe75fc
Martin Jurča <mjurca@centrum.cz>
2015-12-18 13:18:44 +0100

    simplified tests bootstrap

--- test/main.js
+++ test/main.js
@@ -1,15 +1,11 @@
 
-var allProjectFiles = []
 var allTestFiles = []
-var PROJECT_REGEXP = /es6\/.*\.js$/i
 var TEST_REGEXP = /(spec|test)\.js$/i
 
 // load project and tests files and normalize paths to RequireJS module names
 for (var file of Object.keys(window.__karma__.files)) {
   if (TEST_REGEXP.test(file)) {
     allTestFiles.push(pathToModule(file))
-  } else if (PROJECT_REGEXP.test(file)) {
-    allProjectFiles.push(pathToModule(file))
   }
 }
 
@@ -21,7 +17,7 @@ require.config({
   },
 
   // dynamically load all test files
-  deps: allProjectFiles.concat(allTestFiles),
+  deps: allTestFiles,
 
   // we have to kickoff jasmine, as it is asynchronous
   callback: window.__karma__.start
